9c16cdd2157de87318eabc20126f97e4ba2fcd34
Morteza Mohammady Gharasuie <mortezamg63@gmail.com>
2017-10-19 20:30:54 +0330

    Update Adaptive_Median_filter.m

--- Adaptive_Median_filter.m
+++ Adaptive_Median_filter.m
@@ -87,8 +87,8 @@ global imag;
  
  counterPadding=floor((maxPadd-1)/2);
  while(counterPadding)
-     imag=[imag(:,1) imag imag(:,end)];
-     imag=[imag(1,:);imag;imag(end,:)];
+     imag=[imag(:,1) imag imag(:,end)]; % or imag=[imag(:,1,:) imag imag(:,end,:)];
+     imag=[imag(1,:);imag;imag(end,:)]; % or imag=[imag(1,:,:);imag;imag(end,:,:)];
      counterPadding=counterPadding-1;
  end
  output=imag;
